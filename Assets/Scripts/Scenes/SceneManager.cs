79764dda80d91bbe700d394b92d90af8c2b60207
NotRubenchi <al447512@uji.es>
2026-09-26 16:23:46 +0200

    awdawdaw
    dwadawda

---
+++ Assets/Scripts/Scenes/SceneManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManager : MonoBehaviour
+{
+   public void EnterMinigame()
+    {
+        
+        SceneManager.LoadScene(MinijuegoPaella);
+    }
+}
